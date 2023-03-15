1e9d71c7421925a12be2d300866c0c60260183fa
lilai <lilai23@foxmail.com>
2023-03-15 17:49:01 +0800

    主页增加微信二维码

--- docs/.vuepress/components/HomePage.vue
+++ docs/.vuepress/components/HomePage.vue
@@ -4,45 +4,45 @@
       <div class="home-page">
         <div class="content-box">
           <div :class="{ 'logo-box': true, move }">
-            <img :src="$withBase('/img/sermant-logo.png')" alt="Sermant" />
+            <img :src="$withBase('/img/sermant-logo.png')" alt="Sermant"/>
           </div>
           <div class="content">
             <p>{{ $description }}</p>
           </div>
           <div class="button-box">
             <el-button
-              type="danger"
-              @click="goToDocument"
-              icon="el-icon-reading"
-              round
-              style="width:140px"
+                type="danger"
+                @click="goToDocument"
+                icon="el-icon-reading"
+                round
+                style="width:140px"
             >
               {{ quickStart }}
             </el-button>
             <el-button
-              type="blogs"
-              @click="goToBlogs"
-              icon="el-icon-document"
-              style="width:140px"
-              round
+                type="blogs"
+                @click="goToBlogs"
+                icon="el-icon-document"
+                style="width:140px"
+                round
             >
               {{ blogs }}
             </el-button>
             <div class="github-box" @click="goToGithub">
               <div class="img-box">
                 <svg
-                  t="1664449732894"
-                  class="icon"
-                  viewBox="0 0 1024 1024"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  p-id="2540"
-                  width="200"
-                  height="200"
+                    t="1664449732894"
+                    class="icon"
+                    viewBox="0 0 1024 1024"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    p-id="2540"
+                    width="200"
+                    height="200"
                 >
                   <path
-                    d="M512 73.142857q119.428571 0 220.285714 58.857143T892 291.714286 950.857143 512q0 143.428571-83.714286 258T650.857143 928.571429q-15.428571 2.857143-22.857143-4t-7.428571-17.142858q0-1.714286 0.285714-43.714285t0.285714-76.857143q0-55.428571-29.714286-81.142857 32.571429-3.428571 58.571429-10.285715t53.714286-22.285714 46.285714-38 30.285714-60T792 489.142857q0-68-45.142857-117.714286 21.142857-52-4.571429-116.571428-16-5.142857-46.285714 6.285714t-52.571429 25.142857l-21.714285 13.714286q-53.142857-14.857143-109.714286-14.857143t-109.714286 14.857143q-9.142857-6.285714-24.285714-15.428571T330.285714 262.571429 281.714286 254.857143q-25.714286 64.571429-4.571429 116.571428-45.142857 49.714286-45.142857 117.714286 0 48.571429 11.714286 85.714286t30 60 46 38.285714 53.714285 22.285714 58.571429 10.285715q-22.285714 20.571429-28 58.857143-12 5.714286-25.714286 8.571428t-32.571428 2.857143-37.428572-12.285714T276.571429 728q-10.857143-18.285714-27.714286-29.714286t-28.285714-13.714285l-11.428572-1.714286q-12 0-16.571428 2.571428t-2.857143 6.571429 5.142857 8 7.428571 6.857143l4 2.857143q12.571429 5.714286 24.857143 21.714285t18 29.142858l5.714286 13.142857q7.428571 21.714286 25.142857 35.142857t38.285714 17.142857 39.714286 4 31.714286-2l13.142857-2.285714q0 21.714286 0.285714 50.571428t0.285714 31.142857q0 10.285714-7.428571 17.142858t-22.857143 4q-132.571429-44-216.285714-158.571429T73.142857 512q0-119.428571 58.857143-220.285714T291.714286 132 512 73.142857zM239.428571 703.428571q1.714286-4-4-6.857142-5.714286-1.714286-7.428571 1.142857-1.714286 4 4 6.857143 5.142857 3.428571 7.428571-1.142858z m17.714286 19.428572q4-2.857143-1.142857-9.142857-5.714286-5.142857-9.142857-1.714286-4 2.857143 1.142857 9.142857 5.714286 5.714286 9.142857 1.714286z m17.142857 25.714286q5.142857-4 0-10.857143-4.571429-7.428571-9.714285-3.428572-5.142857 2.857143 0 10.285715t9.714285 4z m24 24q4.571429-4.571429-2.285714-10.857143-6.857143-6.857143-11.428571-1.714286-5.142857 4.571429 2.285714 10.857143 6.857143 6.857143 11.428571 1.714286z m32.571429 14.285714q1.714286-6.285714-7.428572-9.142857-8.571429-2.285714-10.857142 4t7.428571 8.571428q8.571429 3.428571 10.857143-3.428571z m36 2.857143q0-7.428571-9.714286-6.285715-9.142857 0-9.142857 6.285715 0 7.428571 9.714286 6.285714 9.142857 0 9.142857-6.285714z m33.142857-5.714286q-1.142857-6.285714-10.285714-5.142857-9.142857 1.714286-8 8.571428t10.285714 4.571429 8-8z"
-                    p-id="2541"
+                      d="M512 73.142857q119.428571 0 220.285714 58.857143T892 291.714286 950.857143 512q0 143.428571-83.714286 258T650.857143 928.571429q-15.428571 2.857143-22.857143-4t-7.428571-17.142858q0-1.714286 0.285714-43.714285t0.285714-76.857143q0-55.428571-29.714286-81.142857 32.571429-3.428571 58.571429-10.285715t53.714286-22.285714 46.285714-38 30.285714-60T792 489.142857q0-68-45.142857-117.714286 21.142857-52-4.571429-116.571428-16-5.142857-46.285714 6.285714t-52.571429 25.142857l-21.714285 13.714286q-53.142857-14.857143-109.714286-14.857143t-109.714286 14.857143q-9.142857-6.285714-24.285714-15.428571T330.285714 262.571429 281.714286 254.857143q-25.714286 64.571429-4.571429 116.571428-45.142857 49.714286-45.142857 117.714286 0 48.571429 11.714286 85.714286t30 60 46 38.285714 53.714285 22.285714 58.571429 10.285715q-22.285714 20.571429-28 58.857143-12 5.714286-25.714286 8.571428t-32.571428 2.857143-37.428572-12.285714T276.571429 728q-10.857143-18.285714-27.714286-29.714286t-28.285714-13.714285l-11.428572-1.714286q-12 0-16.571428 2.571428t-2.857143 6.571429 5.142857 8 7.428571 6.857143l4 2.857143q12.571429 5.714286 24.857143 21.714285t18 29.142858l5.714286 13.142857q7.428571 21.714286 25.142857 35.142857t38.285714 17.142857 39.714286 4 31.714286-2l13.142857-2.285714q0 21.714286 0.285714 50.571428t0.285714 31.142857q0 10.285714-7.428571 17.142858t-22.857143 4q-132.571429-44-216.285714-158.571429T73.142857 512q0-119.428571 58.857143-220.285714T291.714286 132 512 73.142857zM239.428571 703.428571q1.714286-4-4-6.857142-5.714286-1.714286-7.428571 1.142857-1.714286 4 4 6.857143 5.142857 3.428571 7.428571-1.142858z m17.714286 19.428572q4-2.857143-1.142857-9.142857-5.714286-5.142857-9.142857-1.714286-4 2.857143 1.142857 9.142857 5.714286 5.714286 9.142857 1.714286z m17.142857 25.714286q5.142857-4 0-10.857143-4.571429-7.428571-9.714285-3.428572-5.142857 2.857143 0 10.285715t9.714285 4z m24 24q4.571429-4.571429-2.285714-10.857143-6.857143-6.857143-11.428571-1.714286-5.142857 4.571429 2.285714 10.857143 6.857143 6.857143 11.428571 1.714286z m32.571429 14.285714q1.714286-6.285714-7.428572-9.142857-8.571429-2.285714-10.857142 4t7.428571 8.571428q8.571429 3.428571 10.857143-3.428571z m36 2.857143q0-7.428571-9.714286-6.285715-9.142857 0-9.142857 6.285715 0 7.428571 9.714286 6.285714 9.142857 0 9.142857-6.285714z m33.142857-5.714286q-1.142857-6.285714-10.285714-5.142857-9.142857 1.714286-8 8.571428t10.285714 4.571429 8-8z"
+                      p-id="2541"
                   ></path>
                 </svg>
               </div>
@@ -55,6 +55,18 @@
             <Content slot-key="feature2"></Content>
             <Content slot-key="feature3"></Content>
           </div>
+          <div style="background: #EEEEEE">
+            <div class="contact-box">
+              <Content slot-key="Vision"></Content>
+              <Content slot-key="Disclaimer"></Content>
+              <div>
+                <Content slot-key="Contact"></Content>
+                <div class="wechat-QR-box">
+                  <img :src="$withBase('/img/contact-wechat.jpg')" alt="Contact" width=120px height=120px/>
+                </div>
+              </div>
+            </div>
+          </div>
           <Content class="content-footer" slot-key="footer"></Content>
         </div>
       </div>
@@ -66,10 +78,12 @@
   border-color: #cc2229;
   background-color: #cc2229;
 }
+
 .home-page .el-button--danger:hover {
   border-color: #ff0a14;
   background-color: #ff0a14;
 }
+
 .content-box {
   margin: 120px auto 20px;
 }
@@ -88,7 +102,7 @@
   font-size: 16px;
   text-align: center;
   border-top: 1px solid #d2d6e1;
-  margin-top: 110px;
+  background: #EEEEEE
 }
 
 .logo-box {
@@ -98,13 +112,16 @@
   height: 107px;
   margin: 0 auto;
 }
+
 .logo-box > img {
   object-fit: scale-down;
 }
+
 .move {
   transform: translate(0, 100px);
   opacity: 1;
 }
+
 .button-box {
   display: flex;
   align-items: center;
@@ -113,7 +130,9 @@
   margin: 0.8rem auto;
 }
 
-.button-box > button{margin-left:0px !important;}
+.button-box > button {
+  margin-left: 0px !important;
+}
 
 .github-box {
   width: 140px;
@@ -129,9 +148,11 @@
   background-color: #dde2ef73;
   cursor: pointer;
 }
+
 .github-box:hover {
   background-color: #bbbbbb73;
 }
+
 .github-box .img-box {
   border-radius: 50%;
   display: flex;
@@ -140,24 +161,49 @@
   width: 30px;
   height: 30px;
 }
+
 .github-box .star {
   margin-right: 7px;
 }
+
 svg {
   width: 30px;
   height: 30px;
 }
+
 .feature-box {
   display: flex;
   justify-content: space-between;
   width: 64rem;
   margin: 2rem auto 0.8rem;
 }
+
 .feature-box > div {
   margin: 0 45px 20px;
 }
 
-.el-button--blogs,.el-button--blogs:focus,.el-button--blogs.is-active, .el-button--blogs:active{background: #505e86; color: #FFFFFF}
+.contact-box {
+  display: flex;
+  justify-content: space-around;
+  width: 64rem;
+  margin: 2rem auto 0rem;
+  text-align: left;
+  font-size: 12px;
+  color: #24292F
+}
+
+.contact-box > div {
+  margin: 0 45px 0px;
+}
+
+.wechat-QR-box {
+  text-align: center;
+}
+
+.el-button--blogs, .el-button--blogs:focus, .el-button--blogs.is-active, .el-button--blogs:active {
+  background: #505e86;
+  color: #FFFFFF
+}
 
 @media (max-width: 1100px) {
   .feature-box {
@@ -165,15 +211,34 @@ svg {
     width: auto;
   }
 }
+
+@media (max-width: 1100px) {
+  .contact-box {
+    flex-direction: column;
+    width: auto;
+    text-align: left;
+  }
+}
+
+@media (max-width: 1100px) {
+  .wechat-QR-box {
+    flex-direction: column;
+    width: auto;
+    text-align: left;
+  }
+}
+
 @media (max-width: 496px) {
   .logo-box {
     width: 19.84rem;
     height: 4.28rem;
   }
+
   .logo-box > img {
     width: 19.84rem;
     height: 4.28rem;
   }
+
   .content {
     width: 20rem;
   }
@@ -227,13 +292,13 @@ export default {
     },
     getGithubStars() {
       this.$axios
-        .get("https://api.github.com/repos/huaweicloud/sermant")
-        .then((res) => {
-          this.githubStars = res.data.stargazers_count || 0;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+          .get("https://api.github.com/repos/huaweicloud/sermant")
+          .then((res) => {
+            this.githubStars = res.data.stargazers_count || 0;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
     },
   },
 };
--- docs/en/README.md
+++ docs/en/README.md
@@ -16,6 +16,21 @@ Service governance function plug-in, free choice of service governance function.
 Class isolation technology is widely used, so there is no class conflict between framework, plug-in and host class, and developers are unaware of it.
 :::
 
+::: slot Contact
+## Contact
+Scan the QR code below to join the Sermant community for communication.
+:::
+
+::: slot Disclaimer
+## Disclaimer
+Sermant is an open-source project from Huawei Cloud under Apache License 2.0.
+:::
+
+::: slot Vision
+## 愿景
+Sermant's vision is to leverage JavaAgent to provide enhanced service governance capabilities to host applications to address service governance issues in large-scale microservice architectures, and to build a plugin development ecosystem to help developers more easily develop service governance capabilities without interfering with business code.
+:::
+
 ::: slot footer
 Apache-2.0 Licensed | Copyright © 2021-present A Huawei Cloud Project
 :::
